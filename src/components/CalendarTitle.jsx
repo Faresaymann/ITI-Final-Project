aa8f4da13a25f7f8964a9ab9c78a1a950312d8ed
faresayman <123351270+Faresaymann@users.noreply.github.com>
2024-08-10 18:14:52 +0300

    add Sign out

--- src/components/CalendarTitle.jsx
+++ src/components/CalendarTitle.jsx
@@ -5,11 +5,21 @@ const CalendarTitle = () => {
     window.location.reload();
   };
   return (
-    <div onClick={reloadPage} className="cursor-pointer flex items-end mb-9 mt-5 mx-5 px-1 text-white">
+    <div
+      onClick={reloadPage}
+      className="cursor-pointer flex justify-between items-end mb-9 mt-5 mx-5 px-1 text-white"
+    >
+      <div className="flex items-end">
         <i className="fas fa-calendar-alt text-6xl mr-2"></i> {/* Icon */}
-      <h1 className="text-4xl font-bold">Calendar</h1>
+        <h1 className="text-4xl font-bold">Calendar</h1>
+      </div>
+      <div >
+        <Link to="/register" className="text-gray-400 text-2xl font-bold underline">
+          <h1 className="text-3xl font-bold">Sign Out</h1>
+        </Link>
+      </div>
     </div>
-  )
+  );
 };
 
 export default CalendarTitle;
